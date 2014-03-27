1ae8c559e0d05559a83e8bf8292bcf0dedea03ac
mclysek <mclysek@uab.edu>
2014-03-27 09:35:50 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+uabconsultmanager
+=================
+
+This is the development area for the UAB Pathology Consult Manager
+<?php
+
+$db_host = "localhost";
+$db_user = "consultManager";
+$db_pw = "4qN767oPXp2To1uIVi7ZXc1zWQJ0cIesf";
+$db_db = "consultManager";
+
+?>
