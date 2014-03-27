2cad36428eb159daacd2eb471a2d35faef5a8d81
mclysek <mclysek@uab.edu>
2014-03-27 09:35:23 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+uabconsultmanager
+=================
+
+This is the development area for the UAB Pathology Consult Manager
+     <footer>
+       <p><center style="color: #999999;"><strong>etox v0.2</strong> <em>"Baten Kaitos"</em> &mdash; &copy; 2013 The University of Alabama at Birmingham, Department of Pathology</center></p>
+     </footer>
+
+   </div> <!-- /container -->
+
+   <!-- Le javascript
+   ================================================== -->
+   <!-- Placed at the end of the document so the pages load faster -->
+
+
+ </body>
+</html>
